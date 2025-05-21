abdb307a4da5547cf4a02fb1072df2845cbd3794
ishami025 <i.irene@alustudent.com>
2025-05-21 10:56:36 +0200

    my class acrivities

---
+++ first_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="Diana"
+
+echo "Hello, $name!"
+# A sample script to greet the user 
+echo "what is your name?"
+read name
+echo "Hello, $name!"
+echo "Hello, $name! Welcome to shell scripting."
