7928650413c4df233e759ab35757f2ed504b285e
ISHAMI Irené <i.irene@alustudent.com>
2025-12-03 08:18:46 +0200

    Delete first_script.sh

--- first_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-name="Diana"
-
-echo "Hello, $name!"
-# A sample script to greet the user 
-echo "what is your name?"
-read name
-echo "Hello, $name!"
-echo "Hello, $name! Welcome to shell scripting."
